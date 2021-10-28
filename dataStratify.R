0a5bb55a2874d58a4c3fe9cb0b40ec20b72b1465
M2 <robert.qiao@flinders.edu.au>
2021-10-28 12:57:26 +1030

    orig data stratify, security +

---
+++ dataStratify.R
@@ -0,0 +1,21 @@
+library(stringr)
+
+## eligible entries from raw data
+dataCategories <- list("AllocatedGB", "College", "UsageGB", "X_time", "source", "sourcetype")
+
+files <- as.list(list.files(path = 'datafiles-raw/', pattern = '*.csv'))
+
+for (csv in files){
+  inpath <- paste('datafiles-raw/', csv, sep = '')
+  dataset <- read.csv(inpath, sep = ',', header = TRUE)
+
+  for (category in dataCategories) {
+    newDataset[category] <- dataset[category]
+  }
+  
+  ## output file path
+  csv <- gsub(".csv", "-new.csv", csv)
+  outpath <- paste('datafiles/', csv, sep = '')
+  write.csv(newDataset, file=outpath, row.names = FALSE)
+}
+

9df274556d59350be92aa0d9edbaabe56ee942fd
M2 <robert.qiao@flinders.edu.au>
2021-11-19 16:54:20 +1030

    forecast ui <- init

--- storagePredViz/ui.R
+++ storagePredViz/ui.R
@@ -7,36 +7,52 @@ shinyUI(
     dashboardSidebar(
       collapsed = T,
       width = 200,
+      
+      ## -----------------------------------
+      #  Sidebar Menu config
+      ## -----------------------------------
+      
       sidebarMenu(
-        menuItem("storagePredViz", icon = icon("th"), tabName = "menu_top"),
-        menuItem("Github", icon = icon("github"), href = "https://github.com/okiyuki99/ShinyAB"),
-        menuItem("RStudio Cloud", icon = icon("cloud"), href = "https://rstudio.cloud/project/245977"),
-        menuItem("shinyapps.io", icon = icon("external-link-square"), href = "https://okiyuki.shinyapps.io/ShinyAB"),
-        menuItem("About", icon = icon("question-circle-o"), tabName = "menu_about")
+        menuItem("History", icon = icon("layer-group"), tabName = "menu_top"),
+        menuItem("Forcast", icon = icon("eye"), tabName = "menu_forecast"),
+        menuItem("Finance", icon = icon("hand-holding-usd"), tabName = "menu_finance"),
+        menuItem("Github", icon = icon("github"), href = "https://github.com/DeepThoughtHPC/DeepStorage"),
+        menuItem("RStudio Cloud", icon = icon("cloud"), href = "https://rstudio.cloud/"),
+        menuItem("shinyapps.io", icon = icon("external-link-square-alt"), href = "https://okiyuki.shinyapps.io/ShinyAB"),
+        menuItem("About", icon = icon("question-circle"), tabName = "menu_about")
       )
     ),
   
+    ## -----------------------------------
+    #  DashBoard - Main
+    ## -----------------------------------
+    
     dashboardBody(
       theme_grey_light,
       tabItems(
-       tabItem(tabName = "menu_top",
-        fluidRow(
-          box(title = "Parameters", width = 12, solidHeader = T, status = "primary",
-            fluidRow(
-              column(4, 
+        
+        ## -----------------------------------
+        #  DashBoard - History Tab
+        ## -----------------------------------
+        
+        tabItem(tabName = "menu_top",
+          fluidRow(
+            box(title = "Parameters", width = 12, solidHeader = T, status = "primary",
+              fluidRow(
+                column(4, 
                      p(HTML("<b>Time Frame</b>"),span(shiny::icon("info-circle"), id = "info_tf"), dateRangeInput('tf', NULL, start = "2019-09-01")),
                        tippy::tippy_this(elementId = "info_tf",tooltip = "define the time frame for visulization",placement = "right")
                      )
-              ),
-              column(2,
+                ),
+                column(2,
                      p(HTML("<b>Accuracy</b>"),span(shiny::icon("info-circle"), id = "info_acc"), selectInput('acc', NULL, c("Daily", "Weekly", "Monthly", "Quaterly", "Annually")),
                        tippy::tippy_this(elementId = "info_acc",tooltip = "define granulality for visulization'",placement = "right")
                      )
-              ),
-              column(4, 
+                ),
+                column(4, 
                      p(HTML("<b>New Files</b>"),span(shiny::icon("info-circle"), id = "info_file"), fileInput('file', NULL, buttonLabel = "Choose File")),
                        tippy::tippy_this(elementId = "info_file",tooltip = "choose custom time series data",placement = "right")
-                     )
+                ),
             )
           ),
         
@@ -52,26 +68,77 @@ shinyUI(
         #     )
         # ),
         
-        tabBox(
-          title = "Hist", width = 6,
-          id = "dataPlot",
-          tabPanel("HistoryScatter", plotOutput("historyscatter") %>% withSpinner(type = 5)),
-          tabPanel("Historystream", plotlyOutput("historystream") %>% withSpinner(type = 5)),
-          tabPanel("Historyrange", plotlyOutput("historyrange") %>% withSpinner(type = 5)),
-        ),
+          tabBox(
+            title = "Hist", width = 6,
+            id = "dataPlot",
+            tabPanel("HistoryScatter", plotOutput("historyscatter") %>% withSpinner(type = 5)),
+            tabPanel("Historystream", plotlyOutput("historystream") %>% withSpinner(type = 5)),
+            tabPanel("Historyrange", plotlyOutput("historyrange") %>% withSpinner(type = 5)),
+          ),
         
-        tabBox(title = "PCA", width = 6, 
-               id = "HisPCA",
-               tabPanel("PCAseason", plotOutput("historyseason") %>% withSpinner(type = 5)),
-               tabPanel("PCAtrend", plotOutput("historytrend") %>% withSpinner(type = 5)),
-               tabPanel("PCArandom", plotOutput("historyrandom") %>% withSpinner(type = 5)),
-        )
-       ),
+          tabBox(title = "PCA", width = 6, 
+                 id = "HisPCA",
+                 tabPanel("PCAseason", plotOutput("historyseason") %>% withSpinner(type = 5)),
+                 tabPanel("PCAtrend", plotOutput("historytrend") %>% withSpinner(type = 5)),
+                 tabPanel("PCArandom", plotOutput("historyrandom") %>% withSpinner(type = 5)),
+          )
+        ),
       
-       tabItem(tabName = "menu_about",
+      
+        ## -----------------------------------
+        #  DashBoard - About 
+        ## -----------------------------------
+        tabItem(tabName = "menu_about",
               includeMarkdown("docs/about.md")
-       )
+        ),
+      
+        ## -----------------------------------
+        #  DashBoard - Prediction Tab
+        ## -----------------------------------
+    
+        tabItem(tabName = "menu_forecast",
+          fluidRow(
+            box(title = "Forcasting Parameters", width = 12, solidHeader = T, status = "primary",
+              fluidRow(
+                column(4, 
+                       p(HTML("<b>Time Frame</b>"),span(shiny::icon("info-circle"), id = "info_tf"), sliderInput('tf', "Number of forcasting quaters", min = 2, max = 20, 4)),
+                         tippy::tippy_this(elementId = "info_tf",tooltip = "define the time frame for visulization",placement = "right")
+                )
+              ),
+              column(4,
+                  p(HTML("<b>Algorithms</b>"),span(shiny::icon("info-circle"), id = "info_acc"), 
+                    radioButtons('acc', "Prediction Algorithms", c("ARIMA", "LSTM", "LSTM-DNN")),
+                    tippy::tippy_this(elementId = "info_acc",tooltip = "select of prediction algorithms here",placement = "right")
+                  )
+              ),
+              column(5, 
+                  p(HTML("<b>Start Actions</b>"), span(shiny::icon("info-circle"), id = "info_act"), 
+                    submitButton("Start Analysis"),
+                    tippy::tippy_this(elementId = "info_act",tooltip = "submit the selected parameters and start the analysis",placement = "right")
+                  )
+              ),
+            ),
+            
+            ## ------------------
+            #   Prediction Plots
+            ## ------------------
+            
+            box(title = "Future Usage", width = 12,
+                id = "fig-pred",
+                tabPanel("Usage", plotOutput("pred_plot") %>% withSpinner(type = 5))
+            ) 
+          ),
+        ),
+      
+      
+        ## -----------------------------------
+        #  DashBoard - Finance Tab
+        ## -----------------------------------
+      
+        tabItem(tabName = "menu_finance",
+               includeMarkdown("docs/about.md")
+        )
       )
-    ),
+    )
  )
 )
